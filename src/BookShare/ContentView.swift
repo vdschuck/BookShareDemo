88e9d9e3493d2c3c31806b0f6ce037a4bcdbb348
Vinícius Schuck <viniciusschuck@br-epoa-89.local>
2020-01-14 14:24:17 -0300

    Scaffolding

---
+++ src/BookShare/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BookShare
+//
+//  Created by Vinícius Schuck on 14/01/20.
+//  Copyright © 2020 Vinícius Schuck. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}

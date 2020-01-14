d6c224824464f31ecabdee663897e307c5cb82ba
Rafael Rios <rafaelrios@br-epoa-19.local>
2020-01-14 15:23:11 -0200

    adicionada a tela de notificações

--- src/BookShare/View/NotificationView.swift
+++ src/BookShare/View/NotificationView.swift
@@ -9,16 +9,43 @@ struct Notification {
 struct NotificationItem : View {
     @Binding var notification : Notification
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack {
+            //            Image("icon")
+            //                .resizable()
+            //                .frame(width: 80, height: 80, alignment: .center)
+            //                .cornerRadius(20)
+            //            Spacer()
+            VStack {
+                Text(notification.title)
+                    .bold()
+                    .font(.system(size: 18))
+                    .frame(width: UIScreen.main.bounds.width/2.0, height: nil, alignment: .leading)
+                Spacer().frame(height:20)
+                Text(notification.description)
+                    .foregroundColor(.gray)
+                    .frame(width: UIScreen.main.bounds.width/2.0, height: nil, alignment: .leading)
+            }.frame(alignment: .center)
+            Spacer()
+            Image(systemName: "circle.fill")
+                .foregroundColor(.blue)
+        }.padding(20)
+            .onTapGesture {
+                if self.notification.isRead {
+                    self.notification.isRead = false
+                }
+        }
     }
 }
 
 struct NotificationView: View {
-    @State private var notifications : [Notification] = []
+    @State private var notifications : [Notification] = [Notification(title: "Teste", description: "voce recebeu um teste")]
     var body: some View {
-        List {
-            ForEach(notifications.indices) {i in
-                NotificationItem(notification: self.$notifications[i])
+        ScrollView {
+            VStack {
+                ForEach(notifications.indices) {i in
+                    NotificationItem(notification: self.$notifications[i])
+                    Divider()
+                }
             }
         }
     }

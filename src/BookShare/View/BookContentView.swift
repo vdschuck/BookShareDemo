4c5d479f0dab5ca91f1f9d61bf591e219f65a288
Vinícius Schuck <viniciusschuck@br-epoa-89.local>
2020-01-14 16:45:18 -0300

    Book View

--- src/BookShare/View/BookContentView.swift
+++ src/BookShare/View/BookContentView.swift
@@ -2,43 +2,38 @@ import SwiftUI
 
 struct BookContentView: View {
     
-    @Binding var book: Book
+    var book: Book
     
     var body: some View {
-        HStack {
-            Image(self.book.image.first ?? "")
+        VStack {
+            Image(self.book.image.isEmpty ? "book/book_default" : self.book.image[0])
                 .resizable()
-                .frame(width: 60, height: 60, alignment: .center)
+                .frame(width: 140, height: 140, alignment: .center)
                 .cornerRadius(10)
-                .padding(.horizontal, 10)
+                .padding(.horizontal, 15)
             
             VStack {
                 Text(self.book.title)
                     .bold()
-                    .frame(width: UIScreen.main.bounds.width/2.0, height: nil, alignment:.leading)
-                    .font(.system(size: 18))
+                    .frame(width: UIScreen.main.bounds.width/1.0, height: nil, alignment: .center)
+                    .font(.system(size: 20))
                 
                 Text(self.book.author)
                     .foregroundColor(.gray)
-                    .frame(width: UIScreen.main.bounds.width/2.0, height: nil, alignment: .leading)
+                    .frame(width: UIScreen.main.bounds.width/1.0, height: nil, alignment: .center)
+                    .font(.system(size: 14))
+                
+                Text("\(self.book.loanPeriod) dias")
+                    .foregroundColor(.gray)
+                    .frame(width: UIScreen.main.bounds.width/1.0, height: nil, alignment: .center)
                     .font(.system(size: 12))
                 
+                Text("\(self.book.distance) - \(self.book.location)")
+                    .foregroundColor(.gray)
+                    .frame(width: UIScreen.main.bounds.width/2.0, height: nil, alignment: .center)
+                    .font(.system(size: 10))
+               
             }.frame(width: nil, height: 80, alignment: .center)
-            
-            VStack {
-                Button(action: {
-                    print("App Aberto")
-                })
-                {
-                    Text("DETAILS")
-                        .bold()
-                        .foregroundColor(.blue)
-                        .font(.system(size: 16))
-                        .frame(width: 60, height: 30, alignment: .center)
-                        .background(Color.gray.opacity(0.1))
-                        .cornerRadius(20)
-                }.buttonStyle(PlainButtonStyle())
-            }.padding(.leading, 25)
         }
     }
 }
--- src/BookShare/View/BookListView.swift
+++ src/BookShare/View/BookListView.swift
@@ -1,13 +1,16 @@
 import SwiftUI
 
 struct BookListView: View {
+    
+    @State var books: [Book] = [
+        Book(title: "Teste", image: [], author: "Charles", owner: "Vinicius", details: "Número de páginas do livros é de 500.", loanPeriod: 20, location: "Porto Alegre/RS", distance: "5 km"),
+        Book(title: "Teste", image: [], author: "Charles", owner: "Vinicius", details: "Número de páginas do livros é de 500.", loanPeriod: 20, location: "Porto Alegre/RS", distance: "5 km"),
+        Book(title: "Teste", image: [], author: "Charles", owner: "Vinicius", details: "Número de páginas do livros é de 500.", loanPeriod: 20, location: "Porto Alegre/RS", distance: "5 km")
+    ]
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
-}
-
-struct BookListView_Previews: PreviewProvider {
-    static var previews: some View {
-        BookListView()
+        QGrid(books, columns: 2) { item in
+            BookContentView(book: item)
+        }
     }
 }
